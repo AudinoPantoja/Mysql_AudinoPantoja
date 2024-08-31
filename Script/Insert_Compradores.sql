699511cbf4474984de25064a126b0309bdcd5590
AudinoPantoja <pantojabel@gmail.com>
2024-08-30 22:50:59 -0500

    Primer_Subida

---
+++ Script/Insert_Compradores.sql
@@ -0,0 +1,3 @@
+USE prueba;
+INSERT INTO compradores( idcomprador,nombres,apellidos,correo,telefono,direccion,createdAt,updatedAt)
+VALUES (1, "Ramiro", "Perez", "ramiro@gmail.com", "3203442312", "Obrero", "2024-08-27 20:03:00","2024-08-27 20:06:00");
